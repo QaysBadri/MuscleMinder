abea150e584c10c306047f7e8e9ec173e1f51781
levalleyjack <levalleyjack@gmail.com>
2024-04-17 14:32:10 -0700

    navbar and basic pages, setup routing between them

---
+++ my-app/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Hi Team</h1>
+            <p>This is our home page where the muscle groups will be shown.</p>
+        </div>
+    );
+}
+
+export default HomePage;
