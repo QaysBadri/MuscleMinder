40433c5a0bc31797dbc7180c7d82edbf23b2da48
levalleyjack <levalleyjack@gmail.com>
2024-04-23 21:41:13 -0700

    Added chest map, ability to get the muscle name when clicking on it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
+import { useState } from "react";
+import React from 'react';
+import Model, { IExerciseData, IMuscleStats } from 'react-body-highlighter';
+
+import "./index.css";
+
+//This is the main component of the application, if you want to work on the body model, you will need to work on the HomePage component
 function App() {
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -10,9 +18,7 @@ function App() {
         <div id="page-body">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/*
-            <Route path="*" element={<NotFoundPage />} />
-  */}
+            {/* Add additional routes here if needed */}
           </Routes>
         </div>
       </div>
--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Model from 'react-body-highlighter';
+
+import '../App.css';  
 
 function HomePage() {
+    //States 
+    //States are formatted as follows: const [stateName, setStateName] = useState(initialValue);
+    //call setStateName to update the state
+    const [tooltip, setTooltip] = useState("");
+    const [tooltipVisible, setTooltipVisible] = useState(false);
+    
+    //================================================================================================
+
+    /* Testing data, to be replaced with actual data in later steps. Notice how the muscle groups listed
+    in the muscles array are the highlighted muscles. The library does this for us. */
+    const data = [
+        {
+          name: "Bench Press",
+          muscles: ["chest", "triceps", "front-deltoids"]
+        },
+        {
+          name: "Tricep Pushdown",
+          muscles: ["triceps"]
+        }
+    ];
+    //================================================================================================
+
+    //event handlers, these functions are called when the user interacts with the model
+    //event handles are formatted as follows: const handleEventName = (event) => {function body}
+    const handleMouseClick = (muscleData) => {
+        setTooltip(`Muscle: ${muscleData.muscle}`);
+        setTooltipVisible(true);
+
+    };
+
+
+    //================================================================================================
+
+    //return statement, gives the component structure, in an HTML like format
     return (
         <div>
             <h1>Hi Team</h1>
             <p>This is our home page where the muscle groups will be shown.</p>
+            <div class="ModelContainer">
+            <Model
+                data={data}
+                onClick={handleMouseClick} 
+                onMouseOver={handleMouseClick}
+            />
+            <Model
+            type="posterior"
+            data={data}
+            onClick={handleMouseClick} 
+            onMouseOver={handleMouseClick}
+            />
+            </div>
+            {tooltipVisible && (
+                <div className="Tooltip" style={{ position: 'absolute', left: '50%', top: '10%' }}>
+                    {tooltip}
+                </div>
+            )}
         </div>
     );
 }
