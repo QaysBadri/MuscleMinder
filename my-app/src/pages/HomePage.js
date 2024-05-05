9ea48426a937dfe29e68f8b344da990d1b5c02aa
agarc470 <agarc470@ucsc.edu>
2024-05-05 12:36:08 -0700

    implemented dropdown menu for exercises

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -9,8 +9,8 @@ function HomePage() {
     //call setStateName to update the state
     const [tooltip, setTooltip] = useState("");
     const [tooltipVisible, setTooltipVisible] = useState(false);
+    const [selectedExercise, setSelectedExercise] = useState("");
     
-
     /* Testing data, to be replaced with actual data in later steps. Notice how the muscle groups listed
     in the muscles array are the highlighted muscles. The library does this for us. */
     const data = [
@@ -31,13 +31,23 @@ function HomePage() {
         setTooltipVisible(true);
     };
 
+    const handleDropdownChange = (event) => {
+        setSelectedExercise(event.target.value);
+    };    
 
+    const selectedExerciseData = data.find(exercise => exercise.name === selectedExercise);
 
     //return statement, gives the component structure, in an HTML like format
     return (
         <div>
             <h1>Hi Team</h1>
             <p>This is our home page where the muscle groups will be shown.</p>
+            <select value={selectedExercise} onChange={handleDropdownChange} className="ExerciseDropdown">
+                <option value="">Select an Exercise</option>
+                {data.map((exercise, index) => (
+                    <option key={index} value={exercise.name}>{exercise.name}</option>
+                ))}
+            </select>
             <div class="ModelContainer">
             <Model
                 //data for highlighting
