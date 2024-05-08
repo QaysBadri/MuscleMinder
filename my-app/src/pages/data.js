5e6ac649ff9f9b05417be7a6a4ae30acc540018a
levalleyjack <levalleyjack@gmail.com>
2024-05-07 22:44:27 -0700

    dropdown and highlighting muscles based on dropdown selection

---
+++ my-app/src/pages/data.js
@@ -0,0 +1,158 @@
+export default  [
+    {
+        "name": "Barbell Bench Press",
+        "muscles": ["chest", "front-deltoids", "triceps"]
+    },
+    {
+        "name": "Incline Bench Press",
+        "muscles": ["chest", "front-deltoids", "triceps"]
+    },
+    {
+        "name": "Dumbbell Flyes",
+        "muscles": ["chest", "front-deltoids"]
+    },
+    {
+        "name": "Push-ups",
+        "muscles": ["chest", "front-deltoids", "triceps"]
+    },
+    {
+        "name": "Cable Chest Press",
+        "muscles": ["chest", "front-deltoids", "triceps"]
+    },
+    {
+        "name": "Chest Dips",
+        "muscles": ["chest", "front-deltoids", "triceps"]
+    },
+    {
+        "name": "Deadlift",
+        "muscles": ["lower-back", "upper-back", "hamstring", "gluteal"]
+    },
+    {
+        "name": "Pull-ups/Chin-ups",
+        "muscles": ["upper-back", "biceps", "back-deltoids"]
+    },
+    {
+        "name": "Bent-over Rows",
+        "muscles": ["upper-back", "biceps", "back-deltoids"]
+    },
+    {
+        "name": "T-Bar Rows",
+        "muscles": ["upper-back", "biceps", "back-deltoids"]
+    },
+    {
+        "name": "Lat Pulldowns",
+        "muscles": ["upper-back", "biceps", "back-deltoids"]
+    },
+    {
+        "name": "Seated Cable Rows",
+        "muscles": ["upper-back", "biceps", "back-deltoids"]
+    },
+    {
+        "name": "Squats",
+        "muscles": ["quadriceps", "hamstring", "gluteal", "calves"]
+    },
+    {
+        "name": "Lunges",
+        "muscles": ["quadriceps", "hamstring", "gluteal", "calves"]
+    },
+    {
+        "name": "Leg Press",
+        "muscles": ["quadriceps", "hamstring", "gluteal", "calves"]
+    },
+    {
+        "name": "Romanian Deadlifts",
+        "muscles": ["hamstring", "gluteal", "lower-back"]
+    },
+    {
+        "name": "Leg Curls",
+        "muscles": ["hamstring"]
+    },
+    {
+        "name": "Calf Raises",
+        "muscles": ["calves"]
+    },
+    {
+        "name": "Overhead Press",
+        "muscles": ["front-deltoids", "triceps"]
+    },
+    {
+        "name": "Lateral Raises",
+        "muscles": ["front-deltoids"]
+    },
+    {
+        "name": "Front Raises",
+        "muscles": ["front-deltoids"]
+    },
+    {
+        "name": "Upright Rows",
+        "muscles": ["front-deltoids", "biceps", "trapezius"]
+    },
+    {
+        "name": "Arnold Press",
+        "muscles": ["front-deltoids", "triceps"]
+    },
+    {
+        "name": "Face Pulls",
+        "muscles": ["back-deltoids", "upper-back", "trapezius"]
+    },
+    {
+        "name": "Barbell or Dumbbell Bicep Curl",
+        "muscles": ["biceps"]
+    },
+    {
+        "name": "Hammer Curls",
+        "muscles": ["biceps"]
+    },
+    {
+        "name": "Preacher Curls",
+        "muscles": ["biceps"]
+    },
+    {
+        "name": "Concentration Curls",
+        "muscles": ["biceps"]
+    },
+    {
+        "name": "Tricep Dips",
+        "muscles": ["triceps", "chest", "front-deltoids"]
+    },
+    {
+        "name": "Skullcrushers",
+        "muscles": ["triceps"]
+    },
+    {
+        "name": "Tricep Kickbacks",
+        "muscles": ["triceps"]
+    },
+    {
+        "name": "Cable Tricep Pushdowns",
+        "muscles": ["triceps"]
+    },
+    {
+        "name": "Overhead Tricep Extension",
+        "muscles": ["triceps"]
+    },
+    {
+        "name": "Plank",
+        "muscles": ["abs"]
+    },
+    {
+        "name": "Russian Twists",
+        "muscles": ["obliques", "abs"]
+    },
+    {
+        "name": "Bicycle Crunches",
+        "muscles": ["abs", "obliques"]
+    },
+    {
+        "name": "Leg Raises",
+        "muscles": ["abs"]
+    },
+    {
+        "name": "Mountain Climbers",
+        "muscles": ["abs", "front-deltoids"]
+    },
+    {
+        "name": "Woodchoppers",
+        "muscles": ["obliques", "abs"]
+    }
+];
